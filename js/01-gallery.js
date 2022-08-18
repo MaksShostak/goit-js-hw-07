5c1a6a6f832c3bb1f5e30736e08c2df96a274a65
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-18 19:54:35 +0300

    completed the seventh homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector(".gallery");
+const renderGallery = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}""
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+galleryDivRef.insertAdjacentHTML("afterbegin", renderGallery);
+
+galleryDivRef.addEventListener("click", selectGalleryPicture);
+
+const instance = basicLightbox.create(
+  `<img src="" width="800" height="600"></img>`
+);
+
+function selectGalleryPicture(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const modalImgRef = instance.element().querySelector("img");
+  modalImgRef.src = event.target.dataset.source;
+  instance.show();
+  document.addEventListener("keydown", closeModalByEscape);
+
+  function closeModalByEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+    document.removeEventListener("keydown", closeModalByEscape);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryListRef = document.querySelector(".gallery");
+const renderGallery = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(array) {
+  return array
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+galleryListRef.insertAdjacentHTML("afterbegin", renderGallery);
+const imgArrayRef = galleryListRef.querySelectorAll("img");
+console.log(imgArrayRef);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
